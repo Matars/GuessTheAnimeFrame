038cb9420d55764895b6812547389279db2e765d
Matars <khaledmatar19733@gmail.com>
2024-05-12 17:25:29 +0200

    added content

--- api/index.tsx
+++ api/index.tsx
@@ -10,61 +10,97 @@ import { handle } from 'frog/vercel'
 // }
 
 export const app = new Frog({
-  assetsPath: '/',
-  basePath: '/api',
-  // Supply a Hub to enable frame verification.
-  // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
+    assetsPath: '/',
+    basePath: '/api',
+    // Supply a Hub to enable frame verification.
+    // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
 })
 
+let count = 0;
+
+
 app.frame('/', (c) => {
-  const { buttonValue, inputText, status } = c
-  const fruit = inputText || buttonValue
-  return c.res({
-    image: (
-      <div
-        style={{
-          alignItems: 'center',
-          background:
-            status === 'response'
-              ? 'linear-gradient(to right, #432889, #17101F)'
-              : 'black',
-          backgroundSize: '100% 100%',
-          display: 'flex',
-          flexDirection: 'column',
-          flexWrap: 'nowrap',
-          height: '100%',
-          justifyContent: 'center',
-          textAlign: 'center',
-          width: '100%',
-        }}
-      >
-        <div
-          style={{
-            color: 'white',
-            fontSize: 60,
-            fontStyle: 'normal',
-            letterSpacing: '-0.025em',
-            lineHeight: 1.4,
-            marginTop: 30,
-            padding: '0 120px',
-            whiteSpace: 'pre-wrap',
-          }}
-        >
-          {status === 'response'
-            ? `Nice choice.${fruit ? ` ${fruit.toUpperCase()}!!` : ''}`
-            : 'Welcome!'}
-        </div>
-      </div>
-    ),
-    intents: [
-      <TextInput placeholder="Enter custom fruit..." />,
-      <Button value="apples">Apples</Button>,
-      <Button value="oranges">Oranges</Button>,
-      <Button value="bananas">Bananas</Button>,
-      status === 'response' && <Button.Reset>Reset</Button.Reset>,
-    ],
-  })
+    count = 0;
+    return c.res({
+        action: "/Gojo",
+        image: "https://m.media-amazon.com/images/I/718RCq4gL0L.png",
+        intents: [
+            <Button value="apples">Start</Button>, ,
+        ],
+    })
 })
+
+app.frame('/Gojo', (c) => {
+    return c.res({
+        action: "/Killua",
+        image: "https://staticg.sportskeeda.com/editor/2023/06/f2988-16880551809512-1920.jpg",
+        intents: [
+            <Button value="Naruto">Naruto</Button>,
+            <Button value="Gojo">Gojo</Button>,
+            <Button value="Gon">Gon</Button>
+        ],
+    })
+})
+
+app.frame('/Killua', (c) => {
+    const { buttonValue } = c
+
+    if (buttonValue == "Gojo") {
+        count++
+    }
+
+    return c.res({
+        action: "/Gon",
+        image: "https://imgur.com/rppiOqu.jpg",
+        intents: [
+            <Button value="Gon">Gon</Button>,
+            <Button value="Hisoka">Hisoka</Button>,
+            <Button value="Killua">Killua</Button>
+        ],
+    })
+})
+
+app.frame('/Gon', (c) => {
+    const { buttonValue } = c
+
+    if (buttonValue == "Killua") {
+        count++
+    }
+
+    return c.res({
+        action: "/Results",
+        image: "https://i.pinimg.com/originals/dc/5b/de/dc5bdeb3b5e9406a6e725dc77c3a50d0.png",
+        intents: [
+            <Button value="Gon">Gon</Button>,
+            <Button value="Hisoka">Hisoka</Button>,
+            <Button value="Killua">Killua</Button>
+        ],
+    })
+})
+
+app.frame('/Results', (c) => {
+    const { buttonValue } = c
+
+    if (buttonValue == "Gon") {
+        count++
+    }
+
+    return c.res({
+        image: (
+            <div style={{
+                color: 'white',
+                display: 'flex',
+                fontSize: 60,
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh'
+            }}>
+                Results: {count} / 3
+            </div>
+        ),
+    })
+})
+
 
 // @ts-ignore
 const isEdgeFunction = typeof EdgeFunction !== 'undefined'
